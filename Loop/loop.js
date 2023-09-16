4464b45df67757c1639d0ce8794ccadc0c04dc41
SultanMuhammad <sultanrafiq1987@gmail.com>
2023-09-16 19:43:31 +0500

    Loop Function Folder Addition

---
+++ Loop/loop.js
@@ -0,0 +1,80 @@
+"use strict";
+/*for(var i=1;i<101;i++){
+    console.log("Student ID #", i)
+}*/
+/*var cleanestCities = ["Karachi", "Lahore", "Islamabad", "Peshawar"];
+for (var i = 0; i <= 4; i++) {
+  if ("Islamabad" === cleanestCities[i]) {
+    console.log("It's one of the cleanest cities");
+    break;
+  }
+}
+console.log(i);*/
+/*var citrusFruits=["Mango","Lemon","Orange","Kiwi"];
+for(var k=0;k<=4;k++){
+    if("Orange"===citrusFruits[k]){
+        console.log("Orange is citrus Fruit");
+        break;
+    }
+}
+console.log(k);*/
+/*var cleanestCities = ["Karachi", "Lahore", "Islamabad", "Peshawar"];
+var matchFound="no";
+for (var i = 0; i <= 4; i++) {
+  if ("Islamabad" === cleanestCities[i]) {
+    matchFound="yes";
+    alert("It's one of the cleanest cities");
+    
+  }
+}
+if(matchFound==="no") {
+alert("it is not in a list")
+}*/
+/*var cleanestCities = ["Karachi", "Lahore", "Islamabad", "Peshawar"];
+for (var i = 0; i <= 4; i++) {
+  if ("Islamabad" === cleanestCities[i]) {
+    console.log("It's one of the cleanest cities");
+    break;
+  }
+}
+
+var healthyFoods=["Meats","Fish","Green Vegtables","Chicken"];
+for(var h=0;h<=4;h++) {
+  if("Fish"===healthyFoods[h]){
+    console.log("it is one of the Healthiest Food");
+    break;
+  }
+}*/
+/*var cleanestCities = ["Karachi", "Lahore", "Islamabad", "Peshawar"];
+var numelement=cleanestCities.length
+var matchFound="Yes";
+for (var i = 0; i < numelement; i++) {
+  if ("Islamabad" === cleanestCities[i]) {
+    matchFound="Yes"
+    console.log("It's one of the cleanest cities");
+    
+  }
+}
+
+{
+  if(matchFound="No"{
+    console.log("It is not in the list")
+  })
+}
+
+var healthyFoods=["Meats","Fish","Green Vegtables","Chicken"];
+for(var h=0;h<=4;h++) {
+  if("Fish"===healthyFoods[h]){
+    console.log("it is one of the Healthiest Food");
+    break;
+  }
+}*/
+var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+var fullNames = [];
+for (var i = 0; i < firstNames.length; i++) {
+    for (var j = 0; j < lastNames.length; j++) {
+        fullNames.push(firstNames[i] + lastNames[j]);
+    }
+}
+console.log(fullNames);
