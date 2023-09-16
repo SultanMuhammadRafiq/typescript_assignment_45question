fc3345fc2d9ce55fb92d979113acc08d35c92d82
SultanMuhammad <sultanrafiq1987@gmail.com>
2023-09-16 20:17:56 +0500

    Array Function Folder Addition

---
+++ Array/array.ts
@@ -0,0 +1,91 @@
+/*let fruits =["Apple","Banana","Grapes","Strawberry","Orange",true,123]
+console.log(fruits[0]);
+console.log(fruits); 
+console.log(fruits[3]);
+console.log(fruits[5]);*/
+
+/*let fruits =["Apple","Banana","Grapes","Strawberry","Orange",true,123]
+fruits.pop()
+console.log(fruits);
+fruits.pop()
+console.log(fruits);
+fruits.pop()
+console.log(fruits);
+fruits.pop()
+console.log(fruits);*/
+
+/*let fruits =["Apple","Banana","Grapes","Strawberry","Orange",true,123]
+fruits.push("Mango")
+console.log(fruits)
+fruits.push("Kiwi")
+console.log(fruits)
+fruits.push("Car")
+console.log(fruits)*/
+
+/*let fruits =["Apple","Banana","Grapes","Strawberry","Orange",true,123]*/
+
+//*
+
+/*var pets=[];
+pets[0]="Cat";
+pets[1]="Dog";
+pets[2]="Parrots";
+pets[3]="Goat";
+pets[4]="Cow";
+pets[5]="Monkey";
+pets[6]="Deer";
+pets[7]="Horse";
+pets[8]="Eagle";
+console.log(pets);
+pets.pop()
+pets.pop()
+pets.pop()
+pets.push("Pigoen")
+pets.push("Hen")
+
+console.log(pets)*/
+
+/*var pets=[];
+pets[0]="Cat";
+pets[1]="Dog";
+pets[2]="Parrots";
+pets[3]="Goat";
+pets[4]="Cow";
+pets[5]="Monkey";
+pets[6]="Deer";
+pets[7]="Horse";
+pets[8]="Eagle";
+pets.shift()
+pets.shift()
+pets.shift()
+pets.shift()
+pets.unshift("Persian Cat")
+pets.unshift("BullDog")
+pets.unshift("Australian Parrots")
+pets.unshift("Quill")
+console.log(pets)*/
+
+/*var pets=[];
+pets[0]="Cat";
+pets[1]="Dog";
+pets[2]="Parrots";
+pets[3]="Goat";
+pets[4]="Cow";
+pets[5]="Monkey";
+pets[6]="Deer";
+pets[7]="Horse";
+pets[8]="Eagle";
+pets.splice(2,0,"Lion","Elephant","Bear","Tiger","Yak")
+
+console.log(pets)*/
+
+/*let fruits =["Apple","Banana","Grapes","Strawberry","Orange"]
+let a=fruits.slice(3);
+console.log(a);*/
+
+
+
+
+
+
+
