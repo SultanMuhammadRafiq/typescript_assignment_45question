32e28e884911231f2e986bed2cf8d56a76b29778
SultanMuhammad <sultanrafiq1987@gmail.com>
2023-10-03 22:33:59 +0500

    Add Revision Family

---
+++ Revision/revision.js
@@ -0,0 +1,118 @@
+/*console.log("Hello World! I am Sultan");*/
+/*var variable1=1;
+var variable2=2;
+var variable3=3;
+
+
+if(variable1===1){
+   console.log(variable1);
+   if(variable2===1){
+       console.log(variable2)
+   }else{
+       console.log(variable3)
+   }
+}*/
+/*let firstName="Sultan"; // Value contain with Alphabets
+let age=36;// Value contain with numbers
+let isMarried=true; // Value Contain with Boolean True or False
+let score=undefined; // Value contain with Undefined
+let ratio=null; // Value is Unknown*/
+//Templates LIteral 
+/*var myName="Sultan";
+var hello= "Hello " + myName;
+console.log(hello);*/
+/*let myName1="Sultan";
+let myAge=36;
+let myPercent=null;
+let score=true;
+let result=undefined;
+
+console.log(typeof myName1);
+console.log(typeof myAge);
+console.log(typeof myPercent);
+console.log(typeof score);
+console.log(typeof result);*/
+/*Arithmetic operators:
+○ Addition
+Example:*/
+/*let n1 = 1;
+let n2 = 2;
+console.log(n1 + n2); // ?*/
+//Subtract
+/*let n1 = 5;
+let n2 = 2;
+console.log(n1 - n2);*/
+/*let n1 = 5;
+let n2 = 2;
+console.log(n1 * n2);*/
+/*let n1 = 4;
+let n2 = 2;
+console.log(n1 / n2); // ?*/
+/*let n1 = 2;
+let n2 = 5;
+console.log(n1 ** n2);*/
+/*let n1 = 10;
+let n2 = 3;
+console.log(n1 % n2);*/
+/*let n = 1500;
+console.log(n); // 5
+n += 1500;
+console.log(n); // 10
+n -= 500;
+console.log(n); // 5
+n+=5000;
+console.log(n);*/
+/*let n = 5;
+console.log(n == 5); // True
+console.log(n === 5); // True
+console.log(n != 5); // False
+console.log(n > 8); // False
+console.log(n < 8); // True
+console.log(n >= 8); // False
+console.log(n <= 8); // True*/
+/*let n = 5;
+console.log(n >= 5 && n < 10); //
+console.log(n > 5 && n < 10); //
+console.log(n >= 5 || n < 10); //
+console.log(n > 5 || n < 10); //
+console.log(!(n < 10)); //
+console.log(!(n > 10)); // */
+/*function greet(name:string){
+console.log("Hello ! " + name.toUpperCase() + "!!")
+}
+
+greet("sultan");
+greet("sultan");
+greet("sultan");
+greet("sultan");
+greet("sultan");*/
+/*let sum1 = (x: number, y: number): number => {
+    return x + y;
+}
+sum1(11,15);*/
+/*let percent=50;
+if(percent>=97){
+    console.log("Your Grade is A+");
+}else if(percent>=93){
+    console.log("Your Grade is A");
+} else if (percent>=90){
+console.log("Your Grade  is A-");
+}else if (percent>=87){
+    console.log("Your Grade is B+");
+}else if(percent>=83){
+    console.log("Your Grade is B");
+}else if(percent>=80){
+    console.log("Your Grade is B-");
+}else if(percent>=77){
+    console.log("Your Grade is C+");
+}else if(percent>=73){
+    console.log("Your Grade is C");
+}else if(percent>=70){
+    console.log("Your Grade is C-");
+}else if(percent>=67){
+    console.log("Your Grade is D+");
+}else if(percent>=65){
+    console.log("Your Grade is D");
+}else(percent<=65){
+    console.log("Your Grade is E" );
+}*/
